e7765b66728f3154f7c211586662797fd067e9f0
Sven Serneels <svenserneels@gmail.com>
2020-04-06 15:22:01 -0400

    0.9 - requirements aligned

--- src/direpack/__init__.py
+++ src/direpack/__init__.py
@@ -9,8 +9,8 @@ Created on Sun Jul 22 12:17:17 2018
 __name__ = "direpack"
 __author__ = "Sven Serneels"
 __license__ = "MIT"
-__version__ = "0.9.0"
-__date__ = "2020-04-04"
+__version__ = "0.9.1"
+__date__ = "2020-04-06"
 
 from .preprocessing.robcent import VersatileScaler, versatile_scale
 from .preprocessing.gsspp import GenSpatialSignPrePprocessor, gen_ss_pp, gen_ss_covmat
